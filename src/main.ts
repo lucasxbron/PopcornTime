530e559c06881ccb729afa63c7f0994912571926
Lucas <lucasbron@mailbox.org>
2025-02-18 13:29:33 +0100

    feat: update search form layout and remove unused imports

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "./styles/index.css";
-import { displayPopularMedia, getPopularMedia } from "./features/popularMedia";
+// import { displayPopularMedia, getPopularMedia } from "./features/popularMedia";
 import { displayMovieData, searchMovie } from "./features/searchMovie";
 import { setupNavigation, initializePageLogic } from "./features/navigation/router";
 
